63ad85767efbaf490e7976e9c5c569a5710a7637
Gheorghe-Teodor Bercea <gb308@doc.ic.ac.uk>
2014-02-06 12:43:36 +0000

    Add jacobian computations for interior facets.

--- pyop2/pyop2_geometry.h
+++ pyop2/pyop2_geometry.h
@@ -27,9 +27,9 @@
 
 /// Compute Jacobian J for interval embedded in R^3
 #define compute_jacobian_interval_3d(J, vertex_coordinates) \
-  J[0] = vertex_coordinates[3][0] - vertex_coordinates[0][0]; \
-  J[1] = vertex_coordinates[4][0] - vertex_coordinates[1][0]; \
-  J[2] = vertex_coordinates[5][0] - vertex_coordinates[2][0];
+  J[0] = vertex_coordinates[1][0] - vertex_coordinates[0][0]; \
+  J[1] = vertex_coordinates[3][0] - vertex_coordinates[2][0]; \
+  J[2] = vertex_coordinates[5][0] - vertex_coordinates[4][0];
 
 /// Compute Jacobian J for triangle embedded in R^2
 #define compute_jacobian_triangle_2d(J, vertex_coordinates) \
@@ -71,6 +71,78 @@
   J[7] = vertex_coordinates[16][0] - vertex_coordinates[12][0]; \
   J[8] = vertex_coordinates[13][0] - vertex_coordinates[12][0];
 
+// Jacobians for interior facets of different sorts
+
+/// Compute Jacobian J for interval embedded in R^1
+#define compute_jacobian_interval_int_1d compute_jacobian_interval_1d
+
+/// Compute Jacobian J for interval embedded in R^2
+#define compute_jacobian_interval_int_2d(J, vertex_coordinates) \
+  J[0] = vertex_coordinates[1][0] - vertex_coordinates[0][0]; \
+  J[1] = vertex_coordinates[5][0] - vertex_coordinates[4][0];
+
+/// Compute Jacobian J for quad embedded in R^2
+#define compute_jacobian_quad_int_2d(J, vertex_coordinates) \
+  J[0] = vertex_coordinates[1] [0] - vertex_coordinates[0][0]; \
+  J[1] = vertex_coordinates[2] [0] - vertex_coordinates[0][0]; \
+  J[2] = vertex_coordinates[9] [0] - vertex_coordinates[8][0]; \
+  J[3] = vertex_coordinates[10][0] - vertex_coordinates[8][0];
+
+/// Compute Jacobian J for quad embedded in R^3
+#define compute_jacobian_quad_int_3d(J, vertex_coordinates) \
+  J[0] = vertex_coordinates[1] [0] - vertex_coordinates[0] [0]; \
+  J[1] = vertex_coordinates[2] [0] - vertex_coordinates[0] [0]; \
+  J[2] = vertex_coordinates[9] [0] - vertex_coordinates[8] [0]; \
+  J[3] = vertex_coordinates[10][0] - vertex_coordinates[8] [0]; \
+  J[4] = vertex_coordinates[17][0] - vertex_coordinates[16][0]; \
+  J[5] = vertex_coordinates[18][0] - vertex_coordinates[16][0];
+
+/// Compute Jacobian J for interval embedded in R^3
+#define compute_jacobian_interval_int_3d(J, vertex_coordinates) \
+  J[0] = vertex_coordinates[1][0] - vertex_coordinates[0][0]; \
+  J[1] = vertex_coordinates[5][0] - vertex_coordinates[4][0]; \
+  J[2] = vertex_coordinates[9][0] - vertex_coordinates[8][0];
+
+/// Compute Jacobian J for triangle embedded in R^2
+#define compute_jacobian_triangle_int_2d(J, vertex_coordinates) \
+  J[0] = vertex_coordinates[1][0] - vertex_coordinates[0][0]; \
+  J[1] = vertex_coordinates[2][0] - vertex_coordinates[0][0]; \
+  J[2] = vertex_coordinates[7][0] - vertex_coordinates[6][0]; \
+  J[3] = vertex_coordinates[8][0] - vertex_coordinates[6][0];
+
+/// Compute Jacobian J for triangle embedded in R^3
+#define compute_jacobian_triangle_int_3d(J, vertex_coordinates) \
+  J[0] = vertex_coordinates[1] [0] - vertex_coordinates[0] [0]; \
+  J[1] = vertex_coordinates[2] [0] - vertex_coordinates[0] [0]; \
+  J[2] = vertex_coordinates[7] [0] - vertex_coordinates[6] [0]; \
+  J[3] = vertex_coordinates[8] [0] - vertex_coordinates[6] [0]; \
+  J[4] = vertex_coordinates[13][0] - vertex_coordinates[12][0]; \
+  J[5] = vertex_coordinates[14][0] - vertex_coordinates[12][0];
+
+/// Compute Jacobian J for tetrahedron embedded in R^3
+#define compute_jacobian_tetrahedron_int_3d(J, vertex_coordinates) \
+  J[0] = vertex_coordinates[1] [0] - vertex_coordinates[0] [0]; \
+  J[1] = vertex_coordinates[2] [0] - vertex_coordinates[0] [0]; \
+  J[2] = vertex_coordinates[3] [0] - vertex_coordinates[0] [0]; \
+  J[3] = vertex_coordinates[9] [0] - vertex_coordinates[8] [0]; \
+  J[4] = vertex_coordinates[10][0] - vertex_coordinates[8] [0]; \
+  J[5] = vertex_coordinates[11][0] - vertex_coordinates[8] [0]; \
+  J[6] = vertex_coordinates[17][0] - vertex_coordinates[16][0]; \
+  J[7] = vertex_coordinates[18][0] - vertex_coordinates[16][0]; \
+  J[8] = vertex_coordinates[19][0] - vertex_coordinates[16][0];
+
+/// Compute Jacobian J for tensor product prism embedded in R^3
+#define compute_jacobian_prism_int_3d(J, vertex_coordinates) \
+  J[0] = vertex_coordinates[2] [0] - vertex_coordinates[0] [0]; \
+  J[1] = vertex_coordinates[4] [0] - vertex_coordinates[0] [0]; \
+  J[2] = vertex_coordinates[1] [0] - vertex_coordinates[0] [0]; \
+  J[3] = vertex_coordinates[14][0] - vertex_coordinates[12][0]; \
+  J[4] = vertex_coordinates[16][0] - vertex_coordinates[12][0]; \
+  J[5] = vertex_coordinates[13][0] - vertex_coordinates[12][0]; \
+  J[6] = vertex_coordinates[26][0] - vertex_coordinates[24][0]; \
+  J[7] = vertex_coordinates[28][0] - vertex_coordinates[24][0]; \
+  J[8] = vertex_coordinates[25][0] - vertex_coordinates[24][0];
+
 //--- Computation of Jacobian inverses ---
 
 /// Compute Jacobian inverse K for interval embedded in R^1
@@ -103,7 +175,7 @@
 
 /// Compute Jacobian (pseudo)inverse K for triangle embedded in R^3
 #define compute_jacobian_inverse_triangle_3d(K, det, J) \
-  const double d_0 = J[2]*J[5] - J[4]*J[3]; \
+  { const double d_0 = J[2]*J[5] - J[4]*J[3]; \
   const double d_1 = J[4]*J[1] - J[0]*J[5]; \
   const double d_2 = J[0]*J[3] - J[2]*J[1]; \
   const double c_0 = J[0]*J[0] + J[2]*J[2] + J[4]*J[4]; \
@@ -117,7 +189,7 @@
   K[2] = (J[4]*c_1 - J[5]*c_2) / den; \
   K[3] = (J[1]*c_0 - J[0]*c_2) / den; \
   K[4] = (J[3]*c_0 - J[2]*c_2) / den; \
-  K[5] = (J[5]*c_0 - J[4]*c_2) / den;
+  K[5] = (J[5]*c_0 - J[4]*c_2) / den; }
 
 /// Compute Jacobian (pseudo)inverse K for quad embedded in R^2
 #define compute_jacobian_inverse_quad_2d compute_jacobian_inverse_triangle_2d
@@ -127,7 +199,7 @@
 
 /// Compute Jacobian inverse K for tetrahedron embedded in R^3
 #define compute_jacobian_inverse_tetrahedron_3d(K, det, J) \
-  const double d_00 = J[4]*J[8] - J[5]*J[7]; \
+  { const double d_00 = J[4]*J[8] - J[5]*J[7]; \
   const double d_01 = J[5]*J[6] - J[3]*J[8]; \
   const double d_02 = J[3]*J[7] - J[4]*J[6]; \
   const double d_10 = J[2]*J[7] - J[1]*J[8]; \
@@ -145,11 +217,11 @@
   K[5] = d_21 / det; \
   K[6] = d_02 / det; \
   K[7] = d_12 / det; \
-  K[8] = d_22 / det;
+  K[8] = d_22 / det; }
 
 /// Compute Jacobian inverse K for tensor product prism embedded in R^3 - identical to tet
 #define compute_jacobian_inverse_prism_3d(K, det, J) \
-  const double d_00 = J[4]*J[8] - J[5]*J[7]; \
+  { const double d_00 = J[4]*J[8] - J[5]*J[7]; \
   const double d_01 = J[5]*J[6] - J[3]*J[8]; \
   const double d_02 = J[3]*J[7] - J[4]*J[6]; \
   const double d_10 = J[2]*J[7] - J[1]*J[8]; \
@@ -167,7 +239,7 @@
   K[5] = d_21 / det; \
   K[6] = d_02 / det; \
   K[7] = d_12 / det; \
-  K[8] = d_22 / det;
+  K[8] = d_22 / det; }
 
 ///--- Compute facet edge lengths ---
 
